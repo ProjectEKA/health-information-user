5f888639ba9e5260131f6700bdffdead7155d02f
ganesan.arunachalam <aganesan@thoughtworks.com>
2020-01-01 18:16:30 +0530

    [EKA-117] - Contract for user search and create consent request

---
+++ src/main/java/in/org/projecteka/hiu/HiuApplication.java
@@ -0,0 +1,13 @@
+package in.org.projecteka.hiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/org/projecteka/hiu/HiuApplicationTests.java
@@ -0,0 +1,13 @@
+package in.org.projecteka.hiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
