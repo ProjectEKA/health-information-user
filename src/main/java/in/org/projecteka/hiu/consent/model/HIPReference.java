ba80b6d3df443ebb1d62f084d75c9ab1cd5cdf20
chethandeshpande <chethandec22@gmail.com>
2020-02-03 21:43:52 +0530

    [EKA-35] Fixing codacy

--- src/main/java/in/org/projecteka/hiu/consent/model/HIPReference.java
+++ src/main/java/in/org/projecteka/hiu/consent/model/HIPReference.java
@@ -1,10 +1,9 @@
 package in.org.projecteka.hiu.consent.model;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
 
--- src/main/java/in/org/projecteka/hiu/consent/model/PatientLinkedContext.java
+++ src/main/java/in/org/projecteka/hiu/consent/model/PatientLinkedContext.java
@@ -3,9 +3,7 @@ package in.org.projecteka.hiu.consent.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.List;
 
