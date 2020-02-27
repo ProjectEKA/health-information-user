86b51513a0a023ae32566d78213b01827a5b50ba
nexengineer <nexengineer@gmail.com>
2020-02-27 16:45:26 +0530

    Codacy Fixes

--- src/main/java/in/org/projecteka/hiu/DataFlowProperties.java
+++ src/main/java/in/org/projecteka/hiu/DataFlowProperties.java
@@ -1,6 +1,10 @@
 package in.org.projecteka.hiu;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
