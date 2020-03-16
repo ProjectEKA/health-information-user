f38ebfa4af876ca5af9fdd2e760c4c80f0d92c04
Ganesan Arunachalam <ganesan.arunachalam91@gmail.com>
2020-03-16 14:38:16 +0530

    Contract for data notification from HIP (#45)
    * Contract for data notification from HIP
    
    * Address review comments
    
    Co-authored-by: ganesan.arunachalam <ganesan.aruanchalam91@gmail.com>

--- src/main/java/in/org/projecteka/hiu/dataflow/DataFlowController.java
+++ src/main/java/in/org/projecteka/hiu/dataflow/DataFlowController.java
@@ -3,9 +3,11 @@ package in.org.projecteka.hiu.dataflow;
 import in.org.projecteka.hiu.consent.TokenUtils;
 import in.org.projecteka.hiu.dataflow.model.DataNotificationRequest;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -14,6 +16,7 @@ import reactor.core.publisher.Mono;
 public class DataFlowController {
     private DataFlowService dataFlowService;
 
+    @ResponseStatus(HttpStatus.ACCEPTED)
     @PostMapping("/data/notification")
     public Mono<Void> dataNotification(
             @RequestHeader(value = "Authorization") String authorization,
