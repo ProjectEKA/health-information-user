7703a17f6608482fb3a3d03ecf0764ebeb523baa
ganesan.arunachalam <ganesan.aruanchalam91@gmail.com>
2020-03-31 13:52:37 +0530

    Refactor datastore

--- src/main/java/in/org/projecteka/hiu/dataflow/LocalDataStore.java
+++ src/main/java/in/org/projecteka/hiu/dataflow/LocalDataStore.java
@@ -3,7 +3,7 @@ package in.org.projecteka.hiu.dataflow;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import in.org.projecteka.hiu.dataflow.model.DataNotificationRequest;
-import lombok.SneakyThrows;
+import org.apache.log4j.Logger;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
@@ -13,48 +13,52 @@ import java.nio.channels.CompletionHandler;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.Optional;
 
 public class LocalDataStore {
+    private static final Logger logger = Logger.getLogger(LocalDataStore.class);
 
     public Mono<Void> serializeDataToFile(DataNotificationRequest dataNotificationRequest, Path outFileName) {
-        return Mono.create(monoSink -> {
-            byte[] bytes = contentFromRequest(dataNotificationRequest);
-            ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
-            //TODO: find location from application properties. also create the filename under a directory thats relevant to transaction
+        return Mono.create(monoSink ->
+                contentFromRequest(dataNotificationRequest)
+                        .ifPresent(bytes -> {
+                            ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+                            //TODO: find location from application properties.
+                            // also create the filename under a directory that's relevant to transaction
+                            try {
+                                createParentDirectoriesIfNotExists(outFileName);
+                                var channel = AsynchronousFileChannel.open(outFileName,
+                                        StandardOpenOption.CREATE,
+                                        StandardOpenOption.WRITE);
+                                channel.write(byteBuffer, 0, byteBuffer, new CompletionHandler<>() {
+                                    @Override
+                                    public void completed(Integer result, ByteBuffer attachment) {
+                                        monoSink.success();
+                                    }
 
-            createParentDirectoriesIfNotExists(outFileName);
-
-            AsynchronousFileChannel channel = null;
-            try {
-                channel = AsynchronousFileChannel.open(outFileName, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
-            } catch (IOException e) {
-                monoSink.error(e);
-            }
-            channel.write(byteBuffer, 0, byteBuffer, new CompletionHandler<Integer, ByteBuffer>() {
-                @Override
-                public void completed(Integer result, ByteBuffer attachment) {
-                    monoSink.success();
-                }
-                @Override
-                public void failed(Throwable exc, ByteBuffer attachment) {
-                    monoSink.error(exc);
-                }
-            });
-        });
+                                    @Override
+                                    public void failed(Throwable exc, ByteBuffer attachment) {
+                                        monoSink.error(exc);
+                                    }
+                                });
+                            } catch (IOException e) {
+                                logger.error(e);
+                                monoSink.error(e);
+                            }
+                        }));
     }
 
-    @SneakyThrows
-    private void createParentDirectoriesIfNotExists(Path outFileName) {
+    private void createParentDirectoriesIfNotExists(Path outFileName) throws IOException {
         Files.createDirectories(outFileName.getParent());
     }
 
-    private static byte[] contentFromRequest(DataNotificationRequest dataNotificationRequest) {
+    private static Optional<byte[]> contentFromRequest(DataNotificationRequest dataNotificationRequest) {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            return objectMapper.writeValueAsBytes(dataNotificationRequest);
+            return Optional.ofNullable(objectMapper.writeValueAsBytes(dataNotificationRequest));
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
+            logger.error(e);
+            return Optional.empty();
         }
-        return null;
     }
 }
