4f5b09685c228174f476c4ea75acaab7edcb9836
Angshuman Sarkar <angshuonline@gmail.com>
2020-06-03 19:42:32 +0530

    removing redundant code

--- src/main/java/in/org/projecteka/hiu/consent/model/ConsentRequestRepresentation.java
+++ src/main/java/in/org/projecteka/hiu/consent/model/ConsentRequestRepresentation.java
@@ -3,7 +3,6 @@ package in.org.projecteka.hiu.consent.model;
 import in.org.projecteka.hiu.clients.Patient;
 import lombok.SneakyThrows;
 import lombok.Value;
-import org.apache.commons.lang3.time.DateUtils;
 
 import java.time.LocalDateTime;
 
@@ -20,12 +19,6 @@ public class ConsentRequestRepresentation {
     public static ConsentRequestRepresentation toConsentRequestRepresentation(
             Patient patient,
             in.org.projecteka.hiu.consent.model.ConsentRequest consentRequest) {
-//        String[] dateFormats = {"yyyy-MM-dd'T'HH:mm:ss'Z'", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"};
-//        var dateErase = DateUtils.parseDate(
-//                consentRequest.getPermission().getDataEraseAt(),
-//                dateFormats);
-        //var createDate = DateUtils.parseDate(consentRequest.getCreatedDate(), dateFormats);
-
         return new ConsentRequestRepresentation(
                 consentRequest.getId(),
                 new PatientRepresentation(
