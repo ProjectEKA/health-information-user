36edcc0ec6b8543da243d264da4f9e3b97d4f46a
leelanakka <44019459+leelanakka@users.noreply.github.com>
2020-07-14 12:16:38 +0530

    #614 | Leela Prasanth | fixes the Url

--- src/main/java/in/org/projecteka/hiu/consent/PatientConsentController.java
+++ src/main/java/in/org/projecteka/hiu/consent/PatientConsentController.java
@@ -18,7 +18,7 @@ import java.util.Map;
 public class PatientConsentController {
     private final ConsentService consentService;
     public static final String APP_PATH_HIU_CONSENT_REQUESTS = "v1/hiu/patient/consent-request";
-    @PostMapping("APP_PATH_HIU_CONSENT_REQUESTS")
+    @PostMapping(APP_PATH_HIU_CONSENT_REQUESTS)
     @ResponseStatus(HttpStatus.ACCEPTED)
     public Mono<Map<String, String>> postPatientConsentRequest(@RequestBody PatientConsentRequest consentRequest) {
         return ReactiveSecurityContextHolder.getContext()
