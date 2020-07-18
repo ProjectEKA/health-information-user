cf504bcbca2ff40c476349a5ef2ec3226c9a1c5a
Apoorva-GA <apoorvaga29@gmail.com>
2020-07-18 16:23:01 +0530

    [HIU_00] | Fixing consent expiry check

--- src/main/java/in/org/projecteka/hiu/dataflow/HealthInfoManager.java
+++ src/main/java/in/org/projecteka/hiu/dataflow/HealthInfoManager.java
@@ -15,6 +15,8 @@ import reactor.util.function.Tuple2;
 
 import java.time.LocalDateTime;
 import java.util.*;
+import java.time.ZoneOffset;
+import java.util.Map;
 
 import static in.org.projecteka.hiu.ClientError.consentArtefactGone;
 import static in.org.projecteka.hiu.ClientError.invalidHealthInformationRequest;
@@ -81,7 +83,7 @@ public class HealthInfoManager {
 
     private boolean isConsentNotExpired(Map<String, String> consentDetail) {
         var consentExpiryDate = LocalDateTime.parse(consentDetail.get("consentExpiryDate"));
-        return consentExpiryDate.isAfter(LocalDateTime.now());
+        return consentExpiryDate.isAfter(LocalDateTime.now(ZoneOffset.UTC));
     }
 
     private boolean isGrantedConsent(Map<String, String> consentDetail) {
