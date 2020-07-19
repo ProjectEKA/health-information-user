28c2259057d617582e1d6d047929784bca52c69b
Dheeraj Pundir <1236dheeraj.ds@gmail.com>
2020-07-19 17:32:45 +0530

    [PHR-675] | Dheeraj | Add tests

--- src/main/java/in/org/projecteka/hiu/dataflow/model/DataRequestStatusResponse.java
+++ src/main/java/in/org/projecteka/hiu/dataflow/model/DataRequestStatusResponse.java
@@ -3,6 +3,7 @@ package in.org.projecteka.hiu.dataflow.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
 import java.util.List;
--- src/test/java/in/org/projecteka/hiu/dataflow/HealthInfoControllerTest.java
+++ src/test/java/in/org/projecteka/hiu/dataflow/HealthInfoControllerTest.java
@@ -1,15 +1,18 @@
 package in.org.projecteka.hiu.dataflow;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.google.common.util.concurrent.ListenableFuture;
 import com.nimbusds.jose.jwk.JWKSet;
 import in.org.projecteka.hiu.Caller;
 import in.org.projecteka.hiu.DestinationsConfig;
 import in.org.projecteka.hiu.common.Authenticator;
 import in.org.projecteka.hiu.consent.ConceptValidator;
 import in.org.projecteka.hiu.consent.PatientConsentRepository;
-import in.org.projecteka.hiu.dataflow.model.DataPartDetail;
-import in.org.projecteka.hiu.dataflow.model.HealthInfoStatus;
+import in.org.projecteka.hiu.dataflow.model.*;
 import in.org.projecteka.hiu.dataprocessor.DataAvailabilityListener;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -34,6 +37,7 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 import static in.org.projecteka.hiu.common.Constants.API_PATH_FETCH_PATIENT_HEALTH_INFO;
+import static in.org.projecteka.hiu.common.Constants.API_PATH_GET_HEALTH_INFO_STATUS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
@@ -101,14 +105,17 @@ class HealthInfoControllerTest {
     }
 
     @Test
-    void shouldFetchDataPartDetails(){
+    void shouldFetchDataPartDetails() {
         ArgumentCaptor<List<String>> transactionIdsCaptor = ArgumentCaptor.forClass(List.class);
 
         var token = TestBuilders.string();
         var requester = "someone@ncg";
         var caller = new Caller(requester, false, null, true);
         var healthInfoRequest = TestBuilders.healthInformationRequest().limit(10).build();
-        var consentRequestIds = List.of(UUID.randomUUID().toString(), UUID.randomUUID().toString());
+        var dataRequestMappings = TestBuilders.dataRequestMappings(2).stream()
+                .map(PatientDataRequestMapping.PatientDataRequestMappingBuilder::build)
+                .collect(Collectors.toList());
+        var consentRequestIds = dataRequestMappings.stream().map(PatientDataRequestMapping::getConsentRequestId).collect(Collectors.toList());
         var dataPartDetails = TestBuilders.dataPartDetails(2, requester, HealthInfoStatus.SUCCEEDED)
                 .stream().map(DataPartDetail.DataPartDetailBuilder::build).collect(Collectors.toList());
         var transactionIds = dataPartDetails.stream().map(DataPartDetail::getTransactionId).collect(Collectors.toList());
@@ -118,7 +125,7 @@ class HealthInfoControllerTest {
                 "transaction_id", transactionIds.get(0)));
 
         when(authenticator.verify(token)).thenReturn(just(caller));
-        when(patientConsentRepository.fetchConsentRequestIds(healthInfoRequest.getRequestIds())).thenReturn(Flux.fromIterable(consentRequestIds));
+        when(patientConsentRepository.fetchConsentRequestIds(healthInfoRequest.getRequestIds())).thenReturn(Flux.fromIterable(dataRequestMappings));
         when(dataFlowRepository.fetchDataPartDetails(consentRequestIds)).thenReturn(Flux.fromIterable(dataPartDetails));
         when(healthInformationRepository.getHealthInformation(transactionIdsCaptor.capture(), eq(healthInfoRequest.getLimit()), eq(healthInfoRequest.getOffset())))
                 .thenReturn(Flux.fromIterable(healthInfo));
@@ -136,5 +143,66 @@ class HealthInfoControllerTest {
                 .isOk();
 
         assertEquals(Set.copyOf(transactionIds), Set.copyOf(transactionIdsCaptor.getValue()));
+    }
+
+    @Test
+    void shouldReturnStatusesForGivenDataRequestIds() throws JsonProcessingException {
+        var token = TestBuilders.string();
+        var requester = "someone@ncg";
+        var caller = new Caller(requester, false, null, true);
+        var healthInfoRequest = TestBuilders.healthInformationRequest().limit(10).build();
+        var dataRequestMappingBuilder = TestBuilders.dataRequestMapping();
+        var succeededDataConsentReqId = UUID.randomUUID().toString();
+        var partialDataConsentReqId = UUID.randomUUID().toString();
+        var processingDataConsentReqId = UUID.randomUUID().toString();
+
+        var dataRequestMappings = List.of(
+                dataRequestMappingBuilder.consentRequestId(null).build(),
+                dataRequestMappingBuilder.consentRequestId(succeededDataConsentReqId).build(),
+                dataRequestMappingBuilder.consentRequestId(partialDataConsentReqId).build(),
+                dataRequestMappingBuilder.consentRequestId(processingDataConsentReqId).build());
+
+        var consentRequestIds = Set.of(succeededDataConsentReqId, partialDataConsentReqId, processingDataConsentReqId);
+
+        var resultStatuses = dataRequestMappings.stream()
+                .map(mapping -> PatientHealthInfoStatus.builder()
+                        .requestId(mapping.getDataRequestId())
+                        .hipId(mapping.getHipId()))
+                .collect(Collectors.toList());
+
+        ArgumentCaptor<List<String>> consentRequestIdsCaptor = ArgumentCaptor.forClass(List.class);
+
+        var dataPartBuilder = TestBuilders.dataPartDetail().requester(requester);
+        var dataPartDetails = List.of(
+                dataPartBuilder.consentRequestId(succeededDataConsentReqId).status(HealthInfoStatus.SUCCEEDED).build(),
+                dataPartBuilder.consentRequestId(partialDataConsentReqId).status(HealthInfoStatus.ERRORED).build(),
+                dataPartBuilder.consentRequestId(processingDataConsentReqId).status(HealthInfoStatus.PROCESSING).build());
+
+        when(authenticator.verify(token)).thenReturn(just(caller));
+        when(patientConsentRepository.fetchConsentRequestIds(healthInfoRequest.getRequestIds())).thenReturn(Flux.fromIterable(dataRequestMappings));
+        when(dataFlowRepository.fetchDataPartDetails(consentRequestIdsCaptor.capture())).thenReturn(Flux.fromIterable(dataPartDetails));
+
+        var expectedResponse = DataRequestStatusResponse.builder().statuses(List.of(
+                resultStatuses.get(0).status(DataRequestStatus.PROCESSING).build(),
+                resultStatuses.get(1).status(DataRequestStatus.SUCCEEDED).build(),
+                resultStatuses.get(2).status(DataRequestStatus.PARTIAL).build(),
+                resultStatuses.get(3).status(DataRequestStatus.PROCESSING).build()));
+
+        webTestClient
+                .post()
+                .uri(API_PATH_GET_HEALTH_INFO_STATUS)
+                .header("Authorization", token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(healthInfoRequest)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody()
+                .json(new ObjectMapper()
+                        .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+                        .writeValueAsString(expectedResponse));
+
+        assertEquals(Set.copyOf(consentRequestIdsCaptor.getValue()), consentRequestIds);
     }
 }
