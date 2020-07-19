c5e7cae4fdf95ce36947ac492c96710cf4fa686f
Dheeraj Pundir <1236dheeraj.ds@gmail.com>
2020-07-19 17:40:01 +0530

    [PHR-675] | Dheeraj | Fix codacy issues

--- src/main/java/in/org/projecteka/hiu/dataflow/model/DataRequestStatusResponse.java
+++ src/main/java/in/org/projecteka/hiu/dataflow/model/DataRequestStatusResponse.java
@@ -3,7 +3,6 @@ package in.org.projecteka.hiu.dataflow.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.NoArgsConstructor;
 import lombok.Value;
 
 import java.util.List;
