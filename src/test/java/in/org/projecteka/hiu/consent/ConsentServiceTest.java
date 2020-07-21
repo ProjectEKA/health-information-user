d9338344c5ee717695ca808fdb9f7c7f2fa52bfe
Bipul Jha <bipuljha143@gmail.com>
2020-07-21 15:41:16 +0530

    [PHR-627] | Bipul/Prasanth | Add test case for building first Consent Request.

--- src/test/java/in/org/projecteka/hiu/consent/ConsentServiceTest.java
+++ src/test/java/in/org/projecteka/hiu/consent/ConsentServiceTest.java
@@ -11,6 +11,7 @@ import in.org.projecteka.hiu.consent.model.ConsentArtefactResponse;
 import in.org.projecteka.hiu.consent.model.ConsentRequestData;
 import in.org.projecteka.hiu.consent.model.DateRange;
 import in.org.projecteka.hiu.consent.model.GatewayConsentArtefactResponse;
+import in.org.projecteka.hiu.consent.model.PatientConsentRequest;
 import in.org.projecteka.hiu.patient.model.PatientSearchGatewayResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,6 +22,8 @@ import reactor.core.publisher.Mono;
 import reactor.core.publisher.MonoSink;
 import reactor.test.StepVerifier;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -32,6 +35,7 @@ import static in.org.projecteka.hiu.consent.model.ConsentStatus.GRANTED;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -153,5 +157,38 @@ public class ConsentServiceTest {
         StepVerifier.create(publisher).expectComplete().verify();
         verify(consentRepository).insertConsentArtefact(consentDetail, GRANTED, consentRequestId);
         verify(dataFlowRequestPublisher).broadcastDataFlowRequest(consentId, dateRange, signature, dataPushUrl);
+    }
+
+    @Test
+    void shouldBuildFirstConsentRequestIfConsentDataIsEmpty(){
+        String requesterId = randomString();
+        var hiuProperties = hiuProperties().build();
+        var token = randomString();
+        ConsentService consentService = new ConsentService(
+                hiuProperties,
+                consentRepository,
+                dataFlowRequestPublisher,
+                null,
+                null,
+                gateway,
+                healthInformationPublisher,
+                conceptValidator,
+                gatewayServiceClient,
+                patientConsentRepository,
+                consentServiceProperties,
+                patientRequestCache);
+        ConsentRequestData consentRequestData = consentRequestDetails().build();
+        consentRequestData.getConsent().getPatient().setId("hinapatel79@ncg");
+        when(patientConsentRepository.getConsentDetails("10000005",requesterId)).thenReturn(Mono.empty());
+        when(conceptValidator.validatePurpose(anyString())).thenReturn(Mono.just(true));
+        when(gateway.token()).thenReturn(Mono.just(token));
+        when(gatewayServiceClient.sendConsentRequest(eq(token), anyString(), any()))
+                .thenReturn(Mono.empty());
+        when(consentRepository.insertConsentRequestToGateway(any())).thenReturn(Mono.create(MonoSink::success));
+
+
+        Mono<Map<String, String>> request = consentService.handlePatientConsentRequest(requesterId, new PatientConsentRequest(List.of("10000005")));
+
+        StepVerifier.create(request).expectNext(request.block()).verifyComplete();
     }
 }
