4d755125cf47ebc1feff527291e717b127f47de5
Dheeraj Pundir <1236dheeraj.ds@gmail.com>
2020-07-23 20:09:27 +0530

    [HIU_000] | Dheeraj | Remove unused import

--- src/main/java/in/org/projecteka/hiu/SecurityConfiguration.java
+++ src/main/java/in/org/projecteka/hiu/SecurityConfiguration.java
@@ -33,7 +33,6 @@ import static in.org.projecteka.hiu.ClientError.unauthorizedRequester;
 import static in.org.projecteka.hiu.common.Constants.API_PATH_FETCH_PATIENT_HEALTH_INFO;
 import static in.org.projecteka.hiu.common.Constants.API_PATH_GET_HEALTH_INFO_STATUS;
 import static in.org.projecteka.hiu.common.Constants.APP_PATH_PATIENT_CONSENT_REQUEST;
-import static in.org.projecteka.hiu.common.Constants.CM_API_PATH_GET_ATTACHMENT;
 import static in.org.projecteka.hiu.common.Constants.PATH_CONSENTS_HIU_NOTIFY;
 import static in.org.projecteka.hiu.common.Constants.PATH_CONSENTS_ON_FETCH;
 import static in.org.projecteka.hiu.common.Constants.PATH_CONSENTS_ON_FIND;
