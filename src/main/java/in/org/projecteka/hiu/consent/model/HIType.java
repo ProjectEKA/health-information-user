9d2f760c580e2f6f8e908327278015f68b044ae2
leelanakka <44019459+leelanakka@users.noreply.github.com>
2020-08-03 14:59:52 +0530

    679 | Leela Prasanth | Renames the op consultation note

--- src/main/java/in/org/projecteka/hiu/consent/model/HIType.java
+++ src/main/java/in/org/projecteka/hiu/consent/model/HIType.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import java.util.Arrays;
 
 public enum HIType {
-    OP_CONSULTATION_NOTE("OPConsultationNote"),
+    OP_CONSULTATION("OPConsultation"),
     DIAGNOSTIC_REPORT("DiagnosticReport"),
     PRESCRIPTION("Prescription"),
     DISCHARGE_SUMMARY("DischargeSummary");
