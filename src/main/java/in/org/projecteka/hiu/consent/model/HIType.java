0e07385ede639e01baff72ce08167a67ae1dfe19
leelanakka <44019459+leelanakka@users.noreply.github.com>
2020-08-03 11:59:50 +0530

    679 | Leela Prasanth | Adds new Hi types in valuesets and HI Types removes some older un supported types

--- src/main/java/in/org/projecteka/hiu/consent/model/HIType.java
+++ src/main/java/in/org/projecteka/hiu/consent/model/HIType.java
@@ -6,11 +6,8 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import java.util.Arrays;
 
 public enum HIType {
-    CONDITION("Condition"),
-    OBSERVATION("Observation"),
+    OP_CONSULTATION_NOTE("OPConsultationNote"),
     DIAGNOSTIC_REPORT("DiagnosticReport"),
-    MEDICATION_REQUEST("MedicationRequest"),
-    DOCUMENT_REFERENCE("DocumentReference"),
     PRESCRIPTION("Prescription"),
     DISCHARGE_SUMMARY("DischargeSummary");
 
