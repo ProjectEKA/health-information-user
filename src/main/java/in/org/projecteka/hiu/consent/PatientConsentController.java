775381d9be07726a3df1621acb81b0cd352ecf55
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2020-08-10 17:00:49 +0530

    HIU-797 | Wrap status response in an object (#239)

--- src/main/java/in/org/projecteka/hiu/consent/PatientConsentController.java
+++ src/main/java/in/org/projecteka/hiu/consent/PatientConsentController.java
@@ -2,11 +2,11 @@ package in.org.projecteka.hiu.consent;
 
 import in.org.projecteka.hiu.Caller;
 import in.org.projecteka.hiu.consent.model.CareContextInfoRequest;
+import in.org.projecteka.hiu.consent.model.DataTransferStatusResponse;
 import in.org.projecteka.hiu.consent.model.PatientConsentRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
-import java.util.List;
 import java.util.Map;
 
 import static in.org.projecteka.hiu.common.Constants.APP_PATH_PATIENT_CONSENT_REQUEST;
@@ -39,7 +38,8 @@ public class PatientConsentController {
 
     @PostMapping(INTERNAL_PATH_PATIENT_CARE_CONTEXT_INFO)
     @ResponseStatus(HttpStatus.OK)
-    public Mono<List<Map<String, Object>>> careContextStatus(@RequestBody @Valid CareContextInfoRequest request) {
-        return Mono.from(consentService.getLatestCareContextResourceDates(request.getPatientId(), request.getHipId()));
+    public Mono<DataTransferStatusResponse> careContextStatus(@RequestBody @Valid CareContextInfoRequest request) {
+        return consentService.getLatestCareContextResourceDates(request.getPatientId(), request.getHipId())
+                .map(DataTransferStatusResponse::new);
     }
 }
