cccab4d6a5ec7880e4972c98ad28aab32b90e72c
Aditi aggarwal <aggarwal.aditi97@gmail.com>
2020-08-11 19:04:06 +0530

    [HIU_001] Aditi | Add CM Account service offline token authenticator (#243)

--- src/main/java/in/org/projecteka/hiu/common/CMPatientAccountAuthenticator.java
+++ src/main/java/in/org/projecteka/hiu/common/CMPatientAccountAuthenticator.java
@@ -3,19 +3,20 @@ package in.org.projecteka.hiu.common;
 import in.org.projecteka.hiu.Caller;
 import reactor.core.publisher.Mono;
 
-public class CMPatientAccountAuthenticator implements Authenticator{
-    CMAccountServiceAuthenticator cmAccountServiceAuthenticator;
+public class CMPatientAccountAuthenticator implements Authenticator {
+    Authenticator cmAccountServiceAuthenticator;
     CMPatientAuthenticator cmPatientAuthenticator;
 
-    public CMPatientAccountAuthenticator(CMAccountServiceAuthenticator cmAccountServiceAuthenticator,
-                                             CMPatientAuthenticator cmPatientAuthenticator) {
+    public CMPatientAccountAuthenticator(Authenticator cmAccountServiceAuthenticator,
+                                         CMPatientAuthenticator cmPatientAuthenticator) {
         this.cmAccountServiceAuthenticator = cmAccountServiceAuthenticator;
         this.cmPatientAuthenticator = cmPatientAuthenticator;
 
     }
+
     @Override
     public Mono<Caller> verify(String token) {
         return cmAccountServiceAuthenticator.verify(token)
-                .switchIfEmpty(Mono.defer(()-> cmPatientAuthenticator.verify(token)));
+                .switchIfEmpty(Mono.defer(() -> cmPatientAuthenticator.verify(token)));
     }
 }
