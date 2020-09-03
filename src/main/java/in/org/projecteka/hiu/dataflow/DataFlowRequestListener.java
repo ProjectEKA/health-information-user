347758fa075391fc403875c75e4aef80ce84e7e0
Aditi <aggarwal.aditi97@gmail.com>
2020-09-03 10:37:52 +0530

    [Correlation_916] | Fix issue for blocking call in listener

--- src/main/java/in/org/projecteka/hiu/dataflow/DataFlowRequestListener.java
+++ src/main/java/in/org/projecteka/hiu/dataflow/DataFlowRequestListener.java
@@ -92,8 +92,11 @@ public class DataFlowRequestListener {
                                             consentId,
                                             dataFlowRequest)))
                                     .then(defer(() -> dataFlowCache.put(requestId, dataRequestKeyMaterial)));
-                        }).block();
-                MDC.put(Constants.CORRELATION_ID, correlationId);
+                        }).subscriberContext(ctx -> {
+                    Optional<String> traceId = Optional.ofNullable(MDC.get(CORRELATION_ID));
+                    return traceId.map(id -> ctx.put(CORRELATION_ID, id))
+                            .orElseGet(() -> ctx.put(CORRELATION_ID, UUID.randomUUID().toString()));
+                }).subscribe();
                 MDC.clear();
             } catch (Exception exception) {
                 // TODO: Put the message in dead letter queue
