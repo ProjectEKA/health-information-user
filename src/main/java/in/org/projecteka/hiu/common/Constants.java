d281d95e372de1827d3a883e7ac50aa186dcdcc1
mounicasagi <mounica6633@gmail.com>
2020-09-04 12:28:14 +0530

    PHR_Gateway_Token | Get token from PHR Gateway to call account service

--- src/main/java/in/org/projecteka/hiu/common/Constants.java
+++ src/main/java/in/org/projecteka/hiu/common/Constants.java
@@ -22,7 +22,6 @@ public class Constants {
     public static final String CM_API_PATH_GET_ATTACHMENT = "/v1/patient/health-information/fetch/{consent-request-id}/attachments/{file-name}";
     public static final String API_PATH_GET_HEALTH_INFO_STATUS = "/v1/patient/health-information/status";
     public static final String VALIDATE_TOKEN = "/v1/account/token";
-    public static final String HAS_AUTH_ACCESS_TOKEN = "/realms/3scale-sso/protocol/openid-connect/token";
     public static final String INTERNAL_PATH_PATIENT_CARE_CONTEXT_INFO = "/internal/patient/hip/data-transfer-status";
     public static final String GET_CERT = "/v1/auth/cert";
     public static final String PATH_CONSENT_REQUEST_ON_STATUS = CURRENT_VERSION + "/consent-requests/on-status";
