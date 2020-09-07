6dd0899a28269233c1ec9e0e2b4f7943969da1cb
Bipul Jha <bipuljha143@gmail.com>
2020-09-07 18:54:52 +0530

    [InvalidEndPoint] | Bipul | Change Default Error response to 400 (Bad Request)

--- src/main/java/in/org/projecteka/hiu/GlobalExceptionHandler.java
+++ src/main/java/in/org/projecteka/hiu/GlobalExceptionHandler.java
@@ -47,7 +47,7 @@ public class GlobalExceptionHandler extends AbstractErrorWebExceptionHandler {
                 error.getMessage());
         logger.error(message, error);
         // Default error response
-        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         BodyInserter<Object, ReactiveHttpOutputMessage> bodyInserter = fromValue(unknownError().getError());
 
         if (error instanceof ClientError) {
