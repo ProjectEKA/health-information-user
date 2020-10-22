d1be27e07773882b74c2a944d270dcc53359114e
leelanakka <44019459+leelanakka@users.noreply.github.com>
2020-10-22 19:37:08 +0530

    Leela Prasanth | Removes unnecessary annotations

--- src/main/java/in/org/projecteka/hiu/consent/model/consentmanager/ConsentOnNotifyRequest.java
+++ src/main/java/in/org/projecteka/hiu/consent/model/consentmanager/ConsentOnNotifyRequest.java
@@ -3,24 +3,22 @@ package in.org.projecteka.hiu.consent.model.consentmanager;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import in.org.projecteka.hiu.common.GatewayResponse;
 import in.org.projecteka.hiu.common.RespError;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@AllArgsConstructor
 @Builder
-@NoArgsConstructor
 @Data
 public class ConsentOnNotifyRequest {
     private UUID requestId;
     private LocalDateTime timestamp;
     private List<ConsentAcknowledgement> acknowledgement;
     private RespError error;
+    @NonNull
     private GatewayResponse resp;
 }
